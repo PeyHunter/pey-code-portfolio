b719a768fb661561d144f22e9b3fbdcd3060fedc
PeyHunter <Nando.goat@gmail.com>
2026-02-04 10:24:57 +0100

    fix: rename NavBar to Navbar for linux compatibility

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import NavBar from "./components/Navbar";
+import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Projects from "./pages/Projects"; 
 import About from "./pages/About";
@@ -8,7 +8,7 @@ import About from "./pages/About";
 function App() {
   return (
     <Router>
-      <NavBar />
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/projects" element={<Projects />} />
